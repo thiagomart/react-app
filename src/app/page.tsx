852a332e5d4620be0907f11e53c4f14718c4a496
Thiago Martins <thiagomsl@gmail.com>
2024-05-20 00:34:57 -0300

    feat: add components on initial page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,22 @@ export default function Home() {
             <input type="checkbox" name="" id="" />
           </div>
           <div className="ml-4 pb-1 text-xs">
-            <label> Tick this box to confirm you’ve read and agreed to our Terms and Privacy Policy.</label>
+            <label> Tick this box to confirm you've read and agreed to our Terms and Privacy Policy.</label>
+          </div>
+        </div>
+        <div className="flex items-center">
+          <div>
+            <button className="border-0 w-[70px] text-[9px] flex items-center justify-center bg-light-yellow font-normal rounded-full px-4 py-3.5 text-dark-grey" type="submit">
+              <Image src="/lock-icon.svg" alt="back page" width={20} height={20} />
+              <label className="pt-[0.15rem] text-sm font-light cursor-pointer"> Save</label>
+            </button>
+          </div>
+          <div className="pl-5">
+            <a href="https://verify.verifymyage.co.uk/ebay/signup" target="_blank" rel="noopener noreferrer">
+              <button className="border-2 border-black bg-white-smoke md:bg-white block rounded-full px-4 py-3.5 text-dark-grey shadow-sm ring-0 focus:ring-0 hover:ring-0 outline-none placeholder:text-gray-400 sm:text-sm sm:leading-6">
+                Got a VerifyMyAge account? Sign in
+              </button>
+            </a> 
           </div>
         </div>
       </div>
