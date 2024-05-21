b424fe9c659c3da980eddbabf70f91c373a2ab86
Thiago Martins <thiagomsl@gmail.com>
2024-05-21 01:33:50 -0300

    feat: Fix layout Success page Mobile & Desktop

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -16,16 +16,16 @@ export default function Success(): JSX.Element {
         </div>
         <div className="pl-5 md:pl-6 pr-5 pt-5 md:py-12 md:pr-12 lg:pl-[89px] bg-white-smoke md:bg-white h-screen">
           <div>
-            <div className="flex items-center justify-between">
+            <div className="flex items-center justify-between xs:mb-[60px]">
               <Image
-                className="hidden md:block"
+                className=""
                 src="/logo.svg"
                 width={135.1}
                 height={25.5}
                 alt="VerifyMyAge Logo"
               />
             </div>
-            <div className="mb-[60px]">
+            <div className="mb-[60px] hidden md:block">
               <h1 className="mt-[51px] mb-5 hidden md:block">
                 Congrats
               </h1>
@@ -40,35 +40,39 @@ export default function Success(): JSX.Element {
               <div className="relative flex flex-col md:flex-row justify-center items-center bg-light-yellow rounded-2xl md:rounded-3.5xl px-[67px] py-[74px] w-full md:w-96 drop-shadow-2xl md:drop-shadow-3xl z-10">
                 <Image
                   className="hidden md:block"
-                  src="/assert.svg"
+                  src="/assert.svg"  
                   width={75}
                   height={75}
-                  alt="success icon"
-                  priority
+                  alt="Assert icon"
                 />
                 <Image
                   className="md:hidden block"
-                  src="/success_icon.svg"
+                  src="/assert.svg"
                   width={50}
                   height={50}
-                  alt="success icon"
-                  priority
+                  alt="Assert icon"
                 />
-                <div className="relative left-[26.92px]">
-                  <h1 className="text-2xl/[24px] font-light text-left">
+                <div className="md:hidden block pt-[14.82px]">
+                </div>
+                <div className="relative md:left-[26.92px]">
+                  <h1 className="text-2xl/[24px] font-light md:text-left xs:text-center">
                     You've been successfully verified!
                   </h1>
                 </div>
               </div>
               <div className="-mt-5 relative md:hidden pt-14 pb-10 px-5 bg-white rounded-b-2xl drop-shadow z-0">
-                <p className="text-sm font-extralight text-center">
+                <p className="text-sm/[16px] font-extralight text-center">
                   You'll be automatically verified for all future orders.
-                </p>
-                <p className="text-sm font-extralight text-center">
                   Your orders will be dispatched without delay.
                 </p>
               </div>
-              <hr className="mt-[25px] border-foggy-grey md:hidden" />
+              <Image
+                  className="mt-[19px] md:hidden block w-full"
+                  src="/signature-mobile.svg"
+                  width={325}
+                  height={32}
+                  alt="logo"
+                />
             </div>
           </div>
         </div>
